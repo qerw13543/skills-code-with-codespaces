e85bede15f0d2580ece54bf79f5d6641f85afa5c
Hengyu Cang <160857505+qerw13543@users.noreply.github.com>
2026-02-27 00:32:12 +0000

    fix: correct postCreate script

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -1,4 +1,6 @@
+#!/bin/bash
+
 sudo apt-get update
-sudo apt-get install s1
+sudo apt-get install sl
 echo "export PATH=\$PATH:/usr/games" >> ~/.bashrc
 echo "export PATH=\$PATH:/usr/games" >> ~/.zshrc
